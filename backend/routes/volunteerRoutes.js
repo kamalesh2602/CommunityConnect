6faa8fbb69e1b4811432f47a17d1e6c2ccdab6d5
kamalesh2602 <kamaleshg2602@gmail.com>
2026-03-07 15:04:51 +0530

    fix(backend) : volunteer api issues

--- backend/routes/volunteerRoutes.js
+++ backend/routes/volunteerRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { protect, authVolunteer } = require('../middleware/authMiddleware');
 const {
-    registerVolunteer, loginVolunteer, followNGO, getFollowedNGOs
+    registerVolunteer, loginVolunteer, followNGO, getFollowedNGOs, getVerifiedNGOs
 } = require('../controllers/volunteerController');
 
 router.post('/register', registerVolunteer);
 router.post('/login', loginVolunteer);
 
+router.route('/ngos').get(protect, authVolunteer, getVerifiedNGOs);
 router.route('/follow').post(protect, authVolunteer, followNGO);
 router.route('/followed-ngos').get(protect, authVolunteer, getFollowedNGOs);
 
