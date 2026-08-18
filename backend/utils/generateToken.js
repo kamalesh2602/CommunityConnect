a9624f669ccce9e7c3a4445b010cc5188406c4e3
kamalesh2602 <kamaleshg2602@gmail.com>
2026-08-18 14:10:18 +0530

    reduced expiresIn to 1h

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id, role) => {
     return jwt.sign({ id, role }, process.env.JWT_SECRET, {
-        expiresIn: '12h',
+        expiresIn: '1h',
     });
 };
 
